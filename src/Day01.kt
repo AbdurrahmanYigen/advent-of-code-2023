300e0dc8cddd5992e278b3b84f35f756efd68564
Abdurrahman Yigen <abdurrahman.yigen@iits-consulting.de>
2023-12-16 15:59:10 +0100

    Day01 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val regex = Regex("[^0-9]")
+        val result = input.map { it.replace(regex, "") }
+        val finalList: MutableList<Int> = mutableListOf()
+        result.map {
+            finalList.add("${it.first()}${it.last()}".toInt())
+        }
+        var count = 0
+        finalList.forEach { count += it }
+        return count
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+//    fun part2(input: List<String>): Int {
+//        return input.size
+//    }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 77)
 
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+//    part2(input).println()
 }
